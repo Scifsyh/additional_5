e8f42f792b6448f4dd59eb67d3d5034769711b2d
Scifsyh <vjlad9520@gmail.com>
2018-09-15 11:04:22 +0300

    Solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var result = false;
+  var newStr;
+  while (str.length != 0) {
+    newStr = str;
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i][0] == '[' && bracketsConfig[i][1] == ']') {
+        bracketsConfig[i][0] = '\\[';
+        bracketsConfig[i][1] = '\\]';
+      }
+      var key = new RegExp('['+bracketsConfig[i][0]+']'+'['+bracketsConfig[i][1]+']', 'g');
+      str = str.replace(key, '');
+    }
+    if (newStr == str) {
+      return result = false;
+      break;
+    }
+  }
+  return result = true;
 }
